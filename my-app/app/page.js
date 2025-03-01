b4476926a0f1715c8649b2c9e225eafcb1740ef7
Atharva Tamhankar <atharvatamhankar11@gmail.com>
2025-03-01 14:38:12 +0530

    navbar basic

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,9 +1,29 @@
-import Image from "next/image";
+import React from 'react';
+import Navbar from '../Components/Navbar/Navbar';
 
-export default function Home() {
+const Page = () => {
   return (
     <div>
-      Home
+      <Navbar />
+      <section id="home" className="pt-16">
+        <h1>Home Section</h1>
+        <p>Welcome to the Home section.</p>
+
+      </section>
+      <section id="about" className="pt-16">
+        <h1>About Us Section</h1>
+        <p>Learn more about us in this section.</p>
+      </section>
+      <section id="contact" className="pt-16">
+        <h1>Contact Us Section</h1>
+        <p>Get in touch with us in this section.</p>
+      </section>
+      <section id="profile" className="pt-16">
+        <h1>Profile Section</h1>
+        <p>View your profile in this section.</p>
+      </section>
     </div>
   );
-}
+};
+
+export default Page;
